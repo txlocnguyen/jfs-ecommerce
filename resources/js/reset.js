57aa3aec694504ecbfa8cd69b4597135f2a448eb
Silvers <edwardsilvers89@gmail.com>
2022-02-03 09:28:17 -0600

    add reset functionality

---
+++ resources/js/reset.js
@@ -0,0 +1,9 @@
+let resetForm = document.getElementById("reset")
+
+
+resetForm.onclick = function() {
+document.getElementById("newItemName").innerHTML = '';
+document.getElementById("newItemDescription").innerHTML = '';
+document.getElementById("newItemImageUrl").innerHTML = '';
+
+}
